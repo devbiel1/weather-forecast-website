ddcb227141f3a424ea93feda6e5f09e38b4247f5
Gabriel dos Santos <neongenesisevangelionchess@gmail.com>
2025-02-07 00:39:02 -0300

    search bar bug fix

--- script/script.js
+++ script/script.js
@@ -1,18 +1,32 @@
-const apiKey = '70512bad6a894339a2b185548250402';
-const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${"São Paulo"}&lang=pt`;
+function buscar() {
+    var searchValue = document.getElementById('searchInput').value.trim();
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
+    if (searchValue === "") {
+        alert("Digite uma cidade válida!");
+        return;
+    }
 
-        console.log(data)
-        const tempNumberElement = document.getElementById("temp")
-        const conditionTextElement = document.getElementById("condition")
-        const humidityNumberElement = document.getElementById("humidity")
-        const windNumberElement = document.getElementById("wind-speed")
+    const apiKey = '70512bad6a894339a2b185548250402';
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchValue}&lang=pt`;
 
-        tempNumberElement.innerHTML = `${data.current.temp_c}<span style="font-size: 0.5em; vertical-align: super;">°c</span>`
-        conditionTextElement.innerHTML = `${data.current.condition.text}`
-        humidityNumberElement.innerHTML = `${data.current.humidity}%`
-        windNumberElement.innerHTML = `${data.current.wind_kph}Km/h`
-    })
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao buscar dados. Verifique o nome da cidade.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            
+            document.getElementById("temp").innerHTML = `${data.current.temp_c}<span style="font-size: 0.5em; vertical-align: super;">°C</span>`;
+            document.getElementById("condition").innerHTML = data.current.condition.text;
+            document.getElementById("humidity").innerHTML = `${data.current.humidity}%`;
+            document.getElementById("wind-speed").innerHTML = `${data.current.wind_kph} Km/h`;
+
+        })
+        .catch(error => {
+            console.error("Erro ao buscar dados:", error);
+            alert("Não foi possível obter os dados. Verifique o nome da cidade e tente novamente.");
+        });
+}
