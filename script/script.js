e8919adb88b08352eae11969fb28dfaea8bd56bf
Gabriel dos Santos <neongenesisevangelionchess@gmail.com>
2025-02-07 00:50:42 -0300

    removal of unnecessary assets

--- script/script.js
+++ script/script.js
@@ -1,13 +1,19 @@
-function buscar() {
+function buscar(event) {
+
+    // impede que o formulario carregue a pagina;
+    if (event) event.preventDefault();
+
     var searchValue = document.getElementById('searchInput').value.trim();
 
     if (searchValue === "") {
-        alert("Digite uma cidade válida!");
+        alert("Por favor, digite uma cidade!");
         return;
     }
 
     const apiKey = '70512bad6a894339a2b185548250402';
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchValue}&lang=pt`;
+
+    console.log(url)
 
     fetch(url)
         .then(response => {
@@ -17,13 +23,13 @@ function buscar() {
             return response.json();
         })
         .then(data => {
-            console.log(data);
-            
-            document.getElementById("temp").innerHTML = `${data.current.temp_c}<span style="font-size: 0.5em; vertical-align: super;">°C</span>`;
-            document.getElementById("condition").innerHTML = data.current.condition.text;
-            document.getElementById("humidity").innerHTML = `${data.current.humidity}%`;
-            document.getElementById("wind-speed").innerHTML = `${data.current.wind_kph} Km/h`;
 
+            document.getElementById("temp").innerHTML = `${data.current.temp_c}<span style="font-size: 0.5em; vertical-align: super;">°C</span>`;
+            document.getElementById("condition").innerText = data.current.condition.text;
+            document.getElementById("humidity").innerText = `${data.current.humidity}%`;
+            document.getElementById("wind-speed").innerText = `${data.current.wind_kph} Km/h`;
+
+            document.getElementById("weatherIcon").src = data.current.condition.icon;
         })
         .catch(error => {
             console.error("Erro ao buscar dados:", error);
